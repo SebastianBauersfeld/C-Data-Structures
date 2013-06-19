9897dc57f37b12094b930c6933fa6d554c9a9647
Javier Olaechea <pirata@gmail.com>
2013-06-19 13:08:05 -0400

    Refactored to use a linked_list struct in addition to node. Did the insert call

--- linked_list.c
+++ linked_list.c
@@ -2,16 +2,15 @@
 #include <stdio.h>
 #include "linked_list.h"
 
-ll_node* ll_create(int value){
-  ll_node* result = (ll_node *)malloc(sizeof(ll_node));
-  result->value = value;
-  result->next = NULL;
-
-  return result;
+linked_list* ll_create(){
+  linked_list* ll = (linked_list *)malloc(sizeof(linked_list));
+  ll->head = NULL;
+  return ll;
 };
 
-void ll_print(ll_node* node){
-  do {
+void ll_print(linked_list* ll){
+  ll_node* node = ll->head;
+  while(node) {
     printf("%d", node->value);
     if (ll_get_next_node(node) != NULL){
       printf(", ");
@@ -19,18 +18,47 @@ void ll_print(ll_node* node){
       printf("\n");
     };
     node = ll_get_next_node(node);
-  } while(node);
+  };
 };
 
 ll_node* ll_get_next_node(ll_node* node){
   return node->next;
 };
 
+void ll_prepend(linked_list* linked_list, int value){
+  ll_node* new_head = node_create(value, linked_list->head);
+  linked_list->head = new_head;
+};
+
+ll_node* node_create(int value, ll_node* next){
+  ll_node* node = (ll_node*)malloc(sizeof(ll_node));
+  node->value = value;
+  node->next = next;
+  return node;
+};
+
+ll_node* nth_node(linked_list* ll, int position){
+  ll_node* curr_node = ll->head;
+  for(int i = 0; i < position; i++){
+    if(curr_node == NULL) return NULL;
+    curr_node = ll_get_next_node(curr_node);
+  }
+  return curr_node;
+};
+
+void insert_ll(ll_node* new_node, ll_node* prev_node){
+  new_node->next = prev_node->next;
+  prev_node->next = new_node;
+};
+
 int main (int argc, char **args) {
-  ll_node* linked_list = ll_create(3);
-  ll_print(linked_list);
-  /* prepend_ll(linked_list, 5); */
-  /* prepend_ll(linked_list, 11); */
-  /* ll_print(linked_list); */
+  linked_list* ll = ll_create();
+  ll_prepend(ll, 3);
+  ll_print(ll);
+  ll_prepend(ll, 5);
+  ll_prepend(ll, 11);
+  ll_print(ll);
+  insert_ll(node_create(1, NULL), nth_node(ll, 1));
+  ll_print(ll);
   return 0;
 }
--- linked_list.h
+++ linked_list.h
@@ -4,13 +4,20 @@ typedef struct ll_node {
 
 } ll_node;
 
-ll_node* ll_create(int value);
+typedef struct linked_list {
+  ll_node* head;
+} linked_list;
+
+linked_list* ll_create();
+ll_node* node_create(int value, ll_node* next);
 ll_node* ll_get_next_node(ll_node*);
-void ll_print(ll_node* linked_list);
-ll_node* prepend_ll(ll_node* linked_list, int value);
+void ll_print(linked_list* linked_list);
+void ll_prepend(linked_list* linked_list, int value);
 int ll_size(ll_node*);
+void insert_ll(ll_node* new_node, ll_node* prev_node);
+ll_node* nth_node(linked_list* ll, int position);
+/* ll_node* find_value(linked_list* ll, int value); */
 
 /* append_ll */
-/* insert_ll */
+
 /* delete_ll */
-/* find_value */
